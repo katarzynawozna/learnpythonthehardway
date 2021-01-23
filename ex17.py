6fd0689732365309e954be32e5c12ac4f46cec6c
katarzynawozna <malinowyciec@gmail.com>
2021-01-23 15:44:07 +0100

    More Files.

---
+++ ex17.py
@@ -0,0 +1,20 @@
+from sys import argv
+from os.path import exists
+
+script, from_file, to_file = argv
+
+print(f"Copying from {from_file} to {to_file}.")
+indata = (open(from_file)).read()
+
+print(f"The input file is {len(indata)} bytes long.")
+
+print(f"Does the output file exist? {exists(to_file)}.")
+print(f"Ready, hit ENTER to continue, CTRL-C to abort.")
+input()
+
+out_file = open(to_file, 'w')
+out_file.write(indata)
+
+print("All right, all done.")
+
+out_file.close()
