2c6a57dff2ea444b4bffa963162c24c402f582cb
katarzynawozna <malinowyciec@gmail.com>
2021-02-08 13:55:46 +0100

    Study Drills for 33.

--- ex33.py
+++ ex33.py
@@ -1,14 +1,15 @@
-i = 0
-numbers = []
+def fillingup_list(operator, incremention):
+    numbers = []
+    while operator < 6:
+        print(f"At the top i is {operator}.")
+        numbers.append(operator)
 
-while i < 6:
-    print(f"At the top i is {i}.")
-    numbers.append(i)
+        operator += incremention
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {operator}.")
+    return numbers
 
-    i += 1
-    print("Numbers now: ", numbers)
-    print(f"At the bottom i is {i}.")
-
+numbers = fillingup_list(0, 2)
 print("The numbers: ")
 
 for num in numbers:
