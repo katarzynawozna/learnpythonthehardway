b797da4a89e1ec5bda0338b8b37e1a93d256c16a
katarzynawozna <malinowyciec@gmail.com>
2021-02-20 20:20:51 +0100

    Accessing Elements of Lists

---
+++ ex34.py
@@ -0,0 +1,10 @@
+animals = ["bear", "python3.8", "peacock", "kangaroo", "whale", "platypus"]
+
+print("The animal at index 1 is ", animals[1])
+print("The third animal is ", animals[2])
+print("The first animal is ", animals[0])
+print("The animal at index 3 is ", animals[3])
+print("The fifth animal is ", animals[4])
+print("The animal with index 2 is ", animals[2])
+print("The sixth animal is ", animals[5])
+print("The animal with index 4 is ", animals[4])
